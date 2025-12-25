ba0c4789fc1946e6e25ae944d37f7d0c98da5324
Kenenisa Gizaw <152353954+kenenisagizaw@users.noreply.github.com>
2025-12-25 23:32:22 +0300

    Add LoginDto model for user login with Email and Password properties

---
+++ Models/DTOs/LoginDto.cs
@@ -0,0 +1,8 @@
+namespace AddisBookingAdmin.Models.DTOs
+{
+    public class LoginDto
+    {
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
