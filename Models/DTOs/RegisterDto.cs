b99b4909f9fb874174c42d305109bf1771022ae7
Kenenisa Gizaw <152353954+kenenisagizaw@users.noreply.github.com>
2025-12-26 23:33:57 +0300

    Add ConfirmPassword property to RegisterDto for improved registration validation

--- Models/DTOs/RegisterDto.cs
+++ Models/DTOs/RegisterDto.cs
@@ -6,5 +6,6 @@ namespace AddisBookingAdmin.Models.DTOs
         public string FullName { get; set; } = null!;
         public string Email { get; set; } = null!;
         public string Password { get; set; } = null!;
+         public string ConfirmPassword { get; set; } = string.Empty;
     }
 }
