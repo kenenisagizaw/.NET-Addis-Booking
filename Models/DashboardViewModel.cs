9654c518177cdd9eb302bf63f5b8e5907d9108c1
Kenenisa Gizaw <152353954+kenenisagizaw@users.noreply.github.com>
2025-12-29 18:25:10 +0300

    Add DashboardViewModel to encapsulate dashboard data properties

---
+++ Models/DashboardViewModel.cs
@@ -0,0 +1,10 @@
+namespace AddisBookingAdmin.Models
+{
+    public class DashboardViewModel
+    {
+        public int TotalUsers { get; set; }
+        public int TotalProviders { get; set; }
+        public int TotalServices { get; set; }
+        public int TotalPendingProviders { get; set; }
+    }
+}
