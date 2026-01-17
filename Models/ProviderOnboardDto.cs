279f8d190bf6acaed9c681e5a6fee34ccab95a67
Kenenisa Gizaw <152353954+kenenisagizaw@users.noreply.github.com>
2026-01-17 18:09:50 +0300

    Add ProviderOnboardDto model to handle provider onboarding document URLs

---
+++ Models/ProviderOnboardDto.cs
@@ -0,0 +1,10 @@
+// Path: Models/ProviderOnboardDto.cs
+
+namespace AddisBookingAdmin.Models
+{
+    public class ProviderOnboardDto
+    {
+        public string NationalIdDocUrl { get; set; } = default!;
+        public string BusinessDocUrl { get; set; } = default!;
+    }
+}
